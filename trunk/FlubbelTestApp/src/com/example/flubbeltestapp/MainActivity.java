b462343c309c57107ec9eab07985f676712821e3
flohannes@googlemail.com <flohannes@googlemail.com@d34db69d-ee59-b9ac-4427-e2b4895df286>
2014-05-25 19:35:11 +0000

    First Commit

---
+++ trunk/FlubbelTestApp/src/com/example/flubbeltestapp/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.flubbeltestapp;
+
+import java.util.List;
+
+import com.example.flubbeltestapp.model.Accounting;
+import com.example.flubbeltestapp.model.Category;
+import com.example.flubbeltestapp.sqlite.MySQLiteHelper;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    	boolean mboolean = false;
+
+    	SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+    	mboolean = settings.getBoolean("FIRST_RUN", false);
+    	if (!mboolean) {
+    	 // do the thing for the first time 
+    	  settings = getSharedPreferences("PREFS_NAME", 0);
+    	                    SharedPreferences.Editor editor = settings.edit();
+    	                    editor.putBoolean("FIRST_RUN", true);
+    	                    editor.commit(); 
+    	                    
+    	  //Erstelle DatenBank
+	        MySQLiteHelper db = MySQLiteHelper.getInstance(this);
+	        String[] categoryList = {"Lebensmittel","Haushalt","Kostmetik","Geschenke","Kleidung","Schuhe","Technik","Bücher","Büromaterial","Freizeit","Imbiss","Accessoires", "Apotheke","Baumarkt/IKEA","Drogerie","Bahn","Auto"};
+	        for(String c : categoryList){
+	        	Category cNew = new Category(c);
+	        	db.addCategory(cNew);
+	        }
+	        
+//	        List<Category> list = db.getAllCategories();
+//	        for(Category c : list){
+//	        	
+//	        	System.out.println("CName: "+c.getCategoryname());
+//	        }
+    	}    
+
+		TextView ausgabenTextView = (TextView) this.findViewById(R.id.showAusgabenTextView);
+
+		
+    	MySQLiteHelper db = MySQLiteHelper.getInstance(this);
+    	List<Accounting> accountingListe = db.getAllAccountings();
+    	if(accountingListe == null || accountingListe.isEmpty() || accountingListe.size()==0){
+            ausgabenTextView.setText("0 Euro");
+    	} else {
+    		double gesamtAusgabe = 0;
+    		for(Accounting a : accountingListe){
+    			gesamtAusgabe = gesamtAusgabe + a.getValue();
+    		}
+    		ausgabenTextView.setText(gesamtAusgabe + " Euro");
+    	}
+    	
+    	
+        Button addAusgabeButton = (Button) this.findViewById(R.id.AddAusgabeButton);
+        addAusgabeButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(v.getContext(), AusgabeHinzufuegenActivity.class);
+				startActivity(intent);
+			}
+		});
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
---
+++ trunk/FlubbelTestApp/src/com/example/flubbeltestapp/sqlite/MySQLiteHelper.java
@@ -0,0 +1,153 @@
+package com.example.flubbeltestapp.sqlite;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import com.example.flubbeltestapp.model.Accounting;
+import com.example.flubbeltestapp.model.Category;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLiteHelper extends SQLiteOpenHelper{
+
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "AccountingDB";
+	
+	private static final String TABLE_ACCOUNTING = "accounting";
+	private static final String TABLE_CATEGORY = "category";
+	
+	private static final String ACCOUNTING_ID = "id";
+	private static final String ACCOUNTING_VALUE = "value";
+	private static final String ACCOUNTING_CATEGORY_ID = "category_id";
+	private static final String ACCOUNTING_DATE_CREATED = "date_created";
+	private static final String[] ACCOUNTING_COLUMNS = {ACCOUNTING_ID, ACCOUNTING_VALUE, ACCOUNTING_CATEGORY_ID, ACCOUNTING_DATE_CREATED};
+	
+	private static final String CATEGORY_ID = "id";
+	private static final String CATEGORY_CATEGORYNAME = "categoryname";
+	private static final String[] CATEGORY_COLUMNS = {CATEGORY_ID, CATEGORY_CATEGORYNAME};
+
+	private static MySQLiteHelper instance = null;
+	protected MySQLiteHelper(Context context){
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+   public static MySQLiteHelper getInstance(Context context) {
+      if(instance == null) {
+         instance = new MySQLiteHelper(context);
+      }
+      return instance;
+   }
+	   
+	   
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		String CREATE_ACCOUNTING_TABLE = "CREATE TABLE accounting ( id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL, category_id INTEGER, date_created TEXT )";
+		String CREATE_CATEGORY_TABLE = "CREATE TABLE category ( id INTEGER PRIMARY KEY AUTOINCREMENT, categoryname TEXT )";
+		
+		db.execSQL(CREATE_ACCOUNTING_TABLE);
+		db.execSQL(CREATE_CATEGORY_TABLE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		db.execSQL("DROP TABLE IF EXISTS category");
+		db.execSQL("DROP TABLE IF EXISTS accounting");
+		
+		this.onCreate(db);
+	}
+
+	public void addAccounting(Accounting a){
+		Log.d("addAccounting", a.toString());
+		
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(this.ACCOUNTING_VALUE, a.getValue());
+		values.put(this.ACCOUNTING_CATEGORY_ID, a.getCategory_id());
+		values.put(this.ACCOUNTING_DATE_CREATED, a.getDate().toString());
+		
+		db.insert(this.TABLE_ACCOUNTING, null, values);
+		db.close();
+	}
+	
+	public void addCategory(Category c){
+		Log.d("addCategory", c.toString());
+		
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(this.CATEGORY_CATEGORYNAME, c.getCategoryname());
+		
+		db.insert(this.TABLE_CATEGORY, null, values);
+		db.close();
+	}
+	
+	public List<Accounting> getAllAccountings(){
+		List<Accounting> accountingListe = new LinkedList<Accounting>();
+		
+		String query = "SELECT * FROM " + this.TABLE_ACCOUNTING;
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor cursor = db.rawQuery(query, null);
+		
+		Accounting a = null;
+		if(cursor.moveToFirst()){
+			do {
+				a = new Accounting();
+				a.setId(Integer.parseInt(cursor.getString(0)));
+				a.setValue(Double.parseDouble(cursor.getString(1)));
+				a.setCategory_id(Integer.parseInt(cursor.getString(2)));
+				try {
+					a.setDate(new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse(cursor.getString(3)));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				accountingListe.add(a);
+			} while (cursor.moveToNext());
+		}
+		
+		Log.d("getAllAccounting()", accountingListe.toString());
+		return accountingListe;
+	}
+	
+	public List<Category> getAllCategories(){
+		List<Category> categoryListe = new LinkedList<Category>();
+		
+		String query = "SELECT * FROM " + this.TABLE_CATEGORY;
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor cursor = db.rawQuery(query, null);
+		
+		Category c = null;
+		if(cursor.moveToFirst()){
+			do {
+				c = new Category();
+				c.setId(Integer.parseInt(cursor.getString(0)));
+				c.setCategoryname(cursor.getString(1));
+				
+				categoryListe.add(c);
+			} while (cursor.moveToNext());
+		}
+		
+		Log.d("getAllCategories()", categoryListe.toString());
+		return categoryListe;
+	}
+	
+	public void deleteAccounting(Accounting a){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(this.TABLE_ACCOUNTING, this.ACCOUNTING_ID+" = ?", new String[]{String.valueOf(a.getId())});
+		db.close();
+		Log.d("deleteAccounting()", a.toString());
+	}
+	
+}
